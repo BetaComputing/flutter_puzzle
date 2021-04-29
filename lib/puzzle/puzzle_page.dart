373e9f9c3c402afb28ef1c3dde00273cdb30a1b5
aridai <aridai.net@outlook.jp>
2021-04-29 14:54:32 +0900

    パズルゲームのページの読み込み処理を実装

--- lib/puzzle/puzzle_page.dart
+++ lib/puzzle/puzzle_page.dart
@@ -2,40 +2,91 @@ import 'package:flutter/material.dart';
 import 'package:flutter_puzzle/puzzle/puzzle.dart';
 import 'package:flutter_puzzle/puzzle/puzzle_board.dart';
 import 'package:flutter_puzzle/puzzle/puzzle_creator.dart';
+import 'package:flutter_puzzle/puzzle/puzzle_page_bloc.dart';
+import 'package:provider/provider.dart';
 
 /// パズルゲームのページ
 class PuzzlePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     const path = 'assets/puzzle_img.jpg';
-    final settings = PuzzleSettings.defaultSettings;
-    final puzzleFuture = PuzzleCreator().createPuzzle(path, settings).then(
-          (result) => result.when(
-            success: (puzzle) => puzzle,
-            failure: (error) => null,
-          ),
-        );
+
+    return Provider<PuzzlePageBloc>(
+      create: (context) => PuzzlePageBloc(
+        path,
+        PuzzleCreator(),
+      ),
+      dispose: (context, bloc) => bloc.dispose(),
+      child: _PuzzlePage(),
+    );
+  }
+}
+
+class _PuzzlePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _PuzzlePageState();
+}
+
+class _PuzzlePageState extends State<_PuzzlePage> {
+  static const MaxWidth = 500.0;
+
+  @override
+  Widget build(BuildContext context) {
+    final bloc = Provider.of<PuzzlePageBloc>(context);
 
     return Scaffold(
       backgroundColor: Colors.grey,
-      body: Center(
-        child: FutureBuilder<Puzzle?>(
-          future: puzzleFuture,
-          builder: (context, snapshot) {
-            final puzzle = snapshot.data;
+      body: Center(child: _buildBody(bloc)),
+    );
+  }
 
-            if (puzzle == null) return const CircularProgressIndicator();
+  //  ボディ部を生成する。
+  Widget _buildBody(PuzzlePageBloc bloc) {
+    return StreamBuilder<PuzzleLoadingState>(
+      stream: bloc.loadingState,
+      initialData: const PuzzleLoadingState.loading(),
+      builder: (context, snapshot) {
+        final state = snapshot.requireData;
 
-            return ConstrainedBox(
-              constraints: const BoxConstraints(maxWidth: 500),
-              child: PuzzleBoard(
-                puzzle: puzzle,
-                onPieceSelected: (piece) {},
-              ),
-            );
-          },
+        return state.when(
+          loading: () => const CircularProgressIndicator(),
+          success: (puzzle) => _buildPuzzle(bloc, puzzle),
+          failed: () => _buildErrorMessage(bloc),
+        );
+      },
+    );
+  }
+
+  //  パズルのUIを生成する。
+  Widget _buildPuzzle(PuzzlePageBloc bloc, Puzzle puzzle) {
+    return Padding(
+      padding: const EdgeInsets.all(8),
+      child: ConstrainedBox(
+        constraints: const BoxConstraints(maxWidth: MaxWidth),
+        child: PuzzleBoard(
+          puzzle: puzzle,
+          onPieceSelected: bloc.onPieceSelected,
         ),
       ),
+    );
+  }
+
+  //  エラーメッセージを生成する。
+  Widget _buildErrorMessage(PuzzlePageBloc bloc) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text(
+          'パズルの読み込みに失敗しました。',
+          style: TextStyle(color: Colors.red, fontSize: 18),
+        ),
+        const SizedBox(height: 32),
+        ElevatedButton(
+          onPressed: () => bloc.onReloadRequested(),
+          style: ElevatedButton.styleFrom(primary: Colors.redAccent),
+          child: const Text('再読み込み'),
+        ),
+      ],
     );
   }
 }

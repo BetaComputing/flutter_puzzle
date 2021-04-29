a684d9c45ce3cf6a5f5077a6915c821a1d487db8
aridai <aridai.net@outlook.jp>
2021-04-29 10:29:02 +0900

    テスト用にパズル画像の読み込み処理を実装

--- lib/puzzle/puzzle_page.dart
+++ lib/puzzle/puzzle_page.dart
@@ -1,8 +1,54 @@
+import 'dart:ui' as ui;
+
 import 'package:flutter/material.dart';
+import 'package:flutter_puzzle/puzzle/puzzle.dart';
+import 'package:flutter_puzzle/puzzle/puzzle_creator.dart';
 
 /// パズルゲームのページ
 class PuzzlePage extends StatelessWidget {
   @override
-  Widget build(BuildContext context) =>
-      const Scaffold(body: Center(child: Text('flutter_puzzle')));
+  Widget build(BuildContext context) {
+    const path = 'assets/puzzle_img.jpg';
+    final settings = PuzzleSettings.defaultSettings;
+    final puzzleFuture = PuzzleCreator().createPuzzle(path, settings).then(
+          (result) => result.when(
+            success: (puzzle) => puzzle,
+            failure: (error) => null,
+          ),
+        );
+
+    return Scaffold(
+      body: Center(
+        child: FutureBuilder<Puzzle?>(
+          future: puzzleFuture,
+          builder: (context, snapshot) {
+            final puzzleImg = snapshot.data?.img;
+
+            if (puzzleImg == null) return const CircularProgressIndicator();
+
+            return SizedBox(
+              width: puzzleImg.width.toDouble(),
+              height: puzzleImg.height.toDouble(),
+              child: CustomPaint(painter: _PuzzlePainter(puzzleImg)),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class _PuzzlePainter extends CustomPainter {
+  _PuzzlePainter(this.img);
+
+  static final _paint = Paint();
+  final ui.Image img;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    canvas.drawImage(img, Offset.zero, _paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => true;
 }
